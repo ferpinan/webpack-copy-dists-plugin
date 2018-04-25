0b3093fa36174d79bf9a823627b6e84a9135af1a
andoni2222 <andoni2222@gmail.com>
2018-04-25 15:01:15 +0200

    First approach

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const fs = require('fs-extra');
+
+class PrependFile {
+    constructor(from, to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    apply(compiler) {
+        compiler.plugin('done', compilation => {
+            fs.readdir(this.from, (err, files) => {
+                files.forEach(file => {
+                    var filePathFrom = this.from + "/" + file;
+
+                    for(var i in this.to){
+                        var filePathTo = this.to[i] + "/" + file;
+                        var fileStat = fs.lstatSync(filePathFrom);
+
+                        fs.copy(filePathFrom, filePathTo)
+                        // .then(() => console.log('success!'))
+                            .catch(err => console.error(err))
+                    }
+                });
+            });
+        })
+    }
+}
+
+module.exports = PrependFile;
